5c50d0d3d5788b9d490f12ba991d21331fc34299
Dylan Salcedo <salcedo.dylan@gmail.com>
2021-07-18 23:57:46 +0800

    Added linkWebsite to gen_top1000 columns

--- gen_top1000.py
+++ gen_top1000.py
@@ -4,13 +4,13 @@ Generates the top 100 shared channels to be used for a BigQuery job
 
 import pandas as pd
 
-input_file = "data/sharktank-db-201810.csv"
-output_file = "results/top1000_channels_oct2018.csv"
+input_file = "data/sharktank-db-202104.csv"
+output_file = "results/top1000_channels_apr2021.csv"
 
 df = pd.read_csv(input_file)
 df2 = df[~df['linkWebsite'].isna()]
 
-df3 = df2.groupby(['linkEntityId','linkType'],as_index=False).agg(shares=('linkWebsite','count')).sort_values('shares',ascending=False).reset_index(drop=True)
+df3 = df2.groupby(['linkEntityId','linkWebsite','linkType'],as_index=False).agg(shares=('linkWebsite','count')).sort_values('shares',ascending=False).reset_index(drop=True)
 df3 = df3.rename(columns={'linkType':'channelType'})
 
 top = df3.head(1000)
