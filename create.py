be7bca48adc32795e72a02d7fe0d8cccf57276f3
Dylan Salcedo <salcedo.dylan@gmail.com>
2021-08-09 21:26:27 +0800

    Changed create to output linkEntityId

--- create.py
+++ create.py
@@ -9,35 +9,35 @@ from local_utils import get_agents, shared_audience, download_from_gsheets, down
 
 tstart = time.perf_counter()
 
-# Import posts
+## Import posts
 
 # Use this if you want to use local files
 # input_file_posts = "data/sharktank-db-202104.csv"
 # df = pd.read_csv(input_file_posts)
 
 # Use this if you want to get files from gdrive
-input_file_posts_link = "https://drive.google.com/file/d/1nWQtBWrt0hSEP5Zz1kUyKpGMXytD4SSb/view?usp=sharing"
+input_file_posts_link = "https://drive.google.com/file/d/1sE5NLYbI8NP2-00GQCTwm4bFWF0IwteR/view?usp=sharing"
 input_file_posts = download_from_gdrive(input_file_posts_link)
 df = pd.read_csv("Filename.csv")
 
 
 
-# Import pages
+## Import pages
 
 # Use this if you want to use local files
 # input_file_pages = "data/channels_202104_test.csv"
 # top = pd.read_csv(input_file_pages).head(1000)
 
 # Use this if you want to get files from gdrive
-input_file_pages_link = "https://docs.google.com/spreadsheets/d/1iacKMKwpTBTjAk6AyjaXR8mTGLlFJC1s06rL4EIFACs/edit?usp=sharing"
-input_file_pages = download_from_gsheets(input_file_pages_link,sheet='channels_201810')
+input_file_pages_link = "https://docs.google.com/spreadsheets/d/1BLgcBfCWGbcycftwwnvb2WAtGKn42hUat_69gX1YElI/edit?usp=sharing"
+input_file_pages = download_from_gsheets(input_file_pages_link,sheet='channels_202104')
 top = input_file_pages.head(1000)
 top['linkEntityId'] = top['linkEntityId'].astype(int)
 
 
-# Define outputs
-output_file = "results/top1000_links_201810.csv"
-output_link = "https://docs.google.com/spreadsheets/d/1YySi80E3PstrXZF_YKigKRg0RO76MocS-dOAuOiDjmg/edit?usp=sharing"
+## Define outputs
+output_file = "results/top1000_links_202104.csv"
+# output_link = "https://docs.google.com/spreadsheets/d/1YySi80E3PstrXZF_YKigKRg0RO76MocS-dOAuOiDjmg/edit?usp=sharing"
 
 
 # Code proper
@@ -57,8 +57,8 @@ for pair in pairs:
     commons.append(res['commons'])
     if link == True:
         links.append({
-            "site1": pages_df.loc[pair[0],'channelName'],
-            "site2": pages_df.loc[pair[1],'channelName'],
+            "site1": pages_df.loc[pair[0],'linkEntityId'],
+            "site2": pages_df.loc[pair[1],'linkEntityId'],
             "link": res['commons']
         })
     counter+= 1
@@ -70,7 +70,7 @@ for pair in pairs:
 links = pd.DataFrame(links)
 links.to_csv(output_file,index=False)
 links2 = pd.read_csv(output_file)
-export_to_sheet(links2,output_link,sheet_name="top_all")
+# export_to_sheet(links2,output_link,sheet_name="top_all")
 
 tend = time.perf_counter()
 print(f"Time elapsed: {tend-tstart}")
